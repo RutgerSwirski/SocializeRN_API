c20550e7e72034cb781b71b840328f8cfb819c78
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-11-12 14:45:10 +0100

    adding reviews to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    render json: @user
+    @reviews = Review.where(reviewee_id: params[:reviewee_id])
+    render json: @user.to_json( include: [ @reviews ] )
   end
 
   def create
