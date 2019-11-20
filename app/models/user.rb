dd57ffa04723c4f329a61553e6f04069d629d96e
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-11-20 16:49:25 +0000

    dv

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	has_many :invites
 	reverse_geocoded_by :latitude, :longitude
-	after_validation :reverse_geocode
+	# after_validation :reverse_geocode
 	# after_validation :geocode
 end
